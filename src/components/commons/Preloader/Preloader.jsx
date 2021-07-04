ce78013844b5e408cdbb1b201006daa6c58696f9
madyler <madyler@mail.ru>
2021-07-04 18:00:22 +0300

    Updates

--- src/components/commons/Preloader/Preloader.jsx
+++
@@ -1,8 +0,0 @@
-import preloader from "../../../assets/images/Spinner-0.8s-227px.svg";
-import React from "react";
-
-let Preloader = () => {
-    return <img src={preloader} style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}} />
-}
-
-export default Preloader;
--- src/utils/validators/validators.js
+++
@@ -1,7 +0,0 @@
-
-export const maxLengthCreator = (maxLength) => value => {
-return(
-     value.length>maxLength ? `Max lenght is ${maxLength} simbols`:undefined
-    )
-}
-export const required = (value) => (value ? undefined : "Field is required!")
